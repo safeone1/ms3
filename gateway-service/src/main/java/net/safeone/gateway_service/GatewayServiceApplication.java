102149098232d5b8ad0879739c1f6aef837bff50
safeone1 <safeonekan@gmail.com>
2025-10-27 00:13:01 +0100

    added locator config to lower case mapping

--- gateway-service/src/main/java/net/safeone/gateway_service/GatewayServiceApplication.java
+++ gateway-service/src/main/java/net/safeone/gateway_service/GatewayServiceApplication.java
@@ -2,8 +2,7 @@ package net.safeone.gateway_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.health.DiscoveryClientHealthIndicator;
-import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+\import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
 import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
 import org.springframework.context.annotation.Bean;
