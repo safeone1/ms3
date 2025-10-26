5fe5d0cfe44b3c41e5942203e372e95fb0e8527b
safeone1 <safeonekan@gmail.com>
2025-10-26 23:08:19 +0100

    fixed some errors in the entity, now for the gateway

--- inventory-service/src/main/java/net/safeone/inventory_service/entities/Product.java
+++ inventory-service/src/main/java/net/safeone/inventory_service/entities/Product.java
@@ -1,6 +1,8 @@
 package net.safeone.inventory_service.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -11,6 +13,7 @@ import lombok.Setter;
 @AllArgsConstructor @NoArgsConstructor @Getter @Setter @Builder
 public class Product {
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
     private String name;
     private String description;
