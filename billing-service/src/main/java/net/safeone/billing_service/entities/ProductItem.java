51cf8991962d9437a3c700fd8d4423b19a084cee
safeone1 <safeonekan@gmail.com>
2025-10-27 20:27:34 +0100

    ProductItem,Customer,Product added

--- billing-service/src/main/java/net/safeone/billing_service/entities/ProductItem.java
+++ billing-service/src/main/java/net/safeone/billing_service/entities/ProductItem.java
@@ -1,31 +1,32 @@
 package net.safeone.billing_service.entities;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Transient;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import net.safeone.billing_service.model.Customer;
-
+import net.safeone.billing_service.model.Product;
 
 @Entity
 @NoArgsConstructor @AllArgsConstructor @Getter @Setter @Builder
-public class Bill {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+public class ProductItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Date billingDate;
-    private long customerId;
-    @OneToMany(mappedBy = "bill")
-    private List<ProductItem> productItems = new ArrayList<>();
-    @Transient private Customer customer;
+    private String productId;
+    @ManyToOne
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private Bill bill;
+    private int quantity;
+    private double unitPrice;
+    @Transient
+    private Product product;
 }
